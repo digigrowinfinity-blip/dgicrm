5bb566ad17749e4b12d3121292ec66e0fe9ea6c2
digigrowinfinity-blip <digigrowinfinity@gmail.com>
2026-06-27 20:06:48 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { Mail, Lock, Eye, EyeOff, Zap, ArrowRight, CheckCircle } from 'lucide-react'
-import { useApp } from './context/AppContext'
+import { useApp } from '../context/AppContext'
 
 function FacebookIcon() {
   return (
