12629f75f869254e74f9790b77afd7e34946168a
digigrowinfinity-blip <digigrowinfinity@gmail.com>
2026-07-02 09:01:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,28 @@ import LeadDetails from './pages/LeadDetails'
 import Team from './pages/Team'
 import Settings from './pages/Settings'
 import FacebookCallback from './pages/FacebookCallback'
+import ProfileSetup from './pages/ProfileSetup'
+
 function PrivateRoute({ children }) {
   const { user } = useApp()
   return user ? children : <Navigate to="/login" replace />
 }
+
+function ProfileGate({ children }) {
+  const { user } = useApp()
+  if (!user) return <Navigate to="/login" replace />
+  if (!user.business_name) return <Navigate to="/setup-profile" replace />
+  return children
+}
+
 function AppRoutes() {
   const { user } = useApp()
   return (
     <Routes>
       <Route path="/login" element={user ? <Navigate to="/" replace /> : <Login />} />
       <Route path="/facebook-callback" element={<FacebookCallback />} />
-      <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
+      <Route path="/setup-profile" element={<PrivateRoute><ProfileSetup /></PrivateRoute>} />
+      <Route path="/" element={<PrivateRoute><ProfileGate><Layout /></ProfileGate></PrivateRoute>}>
         <Route index element={<Dashboard />} />
         <Route path="leads" element={<Leads />} />
         <Route path="leads/:id" element={<LeadDetails />} />
@@ -30,6 +41,7 @@ function AppRoutes() {
     </Routes>
   )
 }
+
 export default function App() {
   return (
     <AppProvider>
